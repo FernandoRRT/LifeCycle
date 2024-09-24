c5901c9d5d2c2ef2f5bfdc05a314644362218d09
Fernando <suporteiros@gmail.com>
2024-09-23 22:51:17 -0300

    Salvando mudanças antes do rebase

--- app/src/main/java/br/edu/scl/ifsp/sdm/lifecycle/AnotherActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/sdm/lifecycle/AnotherActivity.kt
@@ -2,6 +2,7 @@ package br.edu.scl.ifsp.sdm.lifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import br.edu.scl.ifsp.sdm.lifecycle.databinding.ActivityAnotherBinding
 
 class AnotherActivity : AppCompatActivity() {
@@ -12,5 +13,39 @@ class AnotherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityAnotherBinding.root)
+
+        setSupportActionBar(activityAnotherBinding.toolbarIn.toolbar)
+        supportActionBar?.subtitle = getString(R.string.another)
+
+        Log.v(getString(R.string.app_name), "Another - onCreate(): Início COMPLETO")
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.v(getString(R.string.app_name), "Another - onStart(): Início VISÍVEL")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.v(getString(R.string.app_name), "Another - onResume(): PRIMEIRO PLANO")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.v(getString(R.string.app_name), "Another - onPause(): FIM DO CICLO PRIMEIRO PLANO")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.v(getString(R.string.app_name), "Another - onStop(): fim do CICLO DE VIDA VISÍVEL")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.v(getString(R.string.app_name), "Another - onDestroy(): fim COMPLETO")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.v(getString(R.string.app_name), "Another - onRestart(): preparando OnStart()")
     }
 }
